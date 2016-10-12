71d6e2020ae33278291016d3011491a1ea5ce1fb
Atthaphon Urairat <atthaphon@maqe.com>
2016-10-12 14:26:23 +0700

    Implement #6
    Deleted unused  file and line, also change associated line. (+2 squashed commits)
    Squashed commits:
    [4f65fc7] Use config queue of laravel instead of our Qwatcher cause Qwatcher doesn't have it own queue driver.
    [ec46e23] Facade and update migration use for laravel 5.2
    
    Use config queue of laravel instead of our Qwatcher cause Qwatcher doesn't have it own queue driver.
    
    Deleted unused  file and line, also change associated line.

---
+++ database/migrations/2016_10_20_071609_create_Tracks_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTracksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tracks', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('driver');
+            $table->integer('queue_id');
+            $table->text('payload');
+            $table->integer('attempts');
+            $table->text('meta')->nullable();
+            $table->timestamp('queue_at');
+            $table->timestamp('process_at');
+            $table->timestamp('succeed_at');
+            $table->timestamp('failed_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('tracks');
+    }
+}
