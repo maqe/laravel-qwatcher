46c82118576bb4541181fd6ad7e2075272d29e26
Atthaphon Urairat <atthaphon@maqe.com>
2016-10-12 14:26:23 +0700

    Create base code and folder structure.
    Typo mistake. (+3 squashed commits)
    Squashed commits:
    [1793caa] update .gitignore
    [ca0ba56] Init composer.json, .gitignore, phpunit.xml, Qwatcher.php
    [ca8d61f] Create base code and folder structure.

---
+++ tests/TestCase.php
@@ -0,0 +1,17 @@
+<?php namespace Maqe\Qwatcher\Tests;
+
+use Illuminate\Foundation\Testing\TestCase;
+
+class TestCase extends TestCase
+{
+
+    public function setUp()
+    {
+        parent::setUp();
+    }
+
+    public function tearDown()
+    {
+        parent::tearDown();
+    }
+}
